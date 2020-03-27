fd636f34e7ccfeaacdb5594d67492f4756ccf24d
Enoc Santiago <enocsanti2@gmail.com>
2020-03-27 14:16:37 -0400

    Contoller Done

---
+++ src/main/java/com/revature/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.revature.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.domain.User;
+import com.revature.service.UserService;
+
+@RestController
+public class UserController {
+	private UserService userService;
+
+	@Autowired
+	public void setUserService() {
+		this.userService = userService;
+	}
+	
+	@GetMapping("/user/{username}")
+	public User getUserById(@PathVariable String username) {
+		return userService.getUserById(username);
+	}
+	
+	@PostMapping("/user")
+	public User authUser(@RequestBody User user) {
+		return userService.authUser(user);
+	}
+	
+	@PutMapping("/user")
+	public void updateUser(@RequestBody User user) {
+		userService.updateUser(user);
+	}
+
+}
