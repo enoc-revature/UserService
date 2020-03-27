6647eb847c8017eb82c9c47afdddd6ffc6fe3b6d
Enoc Santiago <enocsanti2@gmail.com>
2020-03-27 13:11:29 -0400

    Initial Push

---
+++ src/test/java/com/revature/UserService/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.UserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
